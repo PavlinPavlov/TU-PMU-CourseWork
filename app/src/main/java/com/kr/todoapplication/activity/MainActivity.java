d8a868c1c9a3336ed7baf87d43a95a4a55ce96d8
Pavlin Pavlov <888pavlin888@gmail.com>
2020-03-21 18:37:52 +0200

    Basic MainActivity button functionality

--- app/src/main/java/com/kr/todoapplication/activity/MainActivity.java
+++ app/src/main/java/com/kr/todoapplication/activity/MainActivity.java
@@ -1,9 +1,11 @@
 package com.kr.todoapplication.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.kr.todoapplication.R;
 
@@ -14,6 +16,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startActivity(new Intent(this, ItemFormActivity.class));
+        Button createButton = findViewById(R.id.m_create_new);
+        Button viewAllButton = findViewById(R.id.m_view_all);
+
+        viewAllButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ItemViewerActivity.class));
+            }
+        });
+
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ItemFormActivity.class));
+            }
+        });
     }
 }
